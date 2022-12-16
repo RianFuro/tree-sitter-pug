5c80bb9ddd92f36517891133f5be77d0d82f6522
Connor McLaughlin <connor1211.mcl@gmail.com>
2022-12-16 16:30:56 +0000

    Docs: add todos for angular templates and standalone class components

--- grammar.js
+++ grammar.js
@@ -2,6 +2,8 @@
 // TODO: support multiple levels of function calls in pug js attrs: `tag(attr=true.call(false.toString()))`
 // TODO: don't break if there are singular { or # in content
 // TODO: add all other types of element to $._children_choice
+// TODO: add support for p(#tplSelect) from angular
+// TODO: check if standalone .class divs are working properly
 module.exports = grammar({
   name: "pug",
   externals: ($) => [$._newline, $._indent, $._dedent],

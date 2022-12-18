d3d4fefe2a8d637d21757b046b9495083367dec5
Connor McLaughlin <connor1211.mcl@gmail.com>
2022-12-18 00:12:12 +0000

    Fix: remove commented code from scanner

--- src/scanner.cc
+++ src/scanner.cc
@@ -15,16 +15,6 @@ struct Scanner {
   unsigned serialize(char *buffer) {
     size_t i = 0;
 
-    // TODO: think about... isn't that problematic?
-    /*
-    size_t stack_size = delimiter_stack.size();
-    if (stack_size > UINT8_MAX) stack_size = UINT8_MAX;
-    buffer[i++] = stack_size;
-
-    memcpy(&buffer[i], delimiter_stack.data(), stack_size);
-    i += stack_size;
-    */
-
     vector<uint16_t>::iterator iter = indent_length_stack.begin() + 1,
                                end = indent_length_stack.end();
 
